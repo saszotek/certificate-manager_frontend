8a41d13b4c7bc3a9d1f80e8d967e6b3468ea2cd5
saszotek <danielzielinski789@gmail.com>
2022-10-21 12:27:53 +0200

    cors config

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Home() {
+  const reqBody = {
+    username: "pies",
+    password: "1234",
+  };
+
+  fetch("api/auth/login", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "post",
+    body: JSON.stringify(reqBody),
+  })
+    .then((response) => Promise.all([response.json(), response.headers]))
+    .then(([body, headers]) => {
+      const authValue = headers.get("authorization");
+      console.log(authValue);
+      console.log(body);
+    });
+
   return (
     <div>
       <div>Homeee</div>
