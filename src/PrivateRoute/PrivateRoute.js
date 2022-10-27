279ba4577c5faa82acae45311dacb413bf5c33ec
saszotek <danielzielinski789@gmail.com>
2022-10-27 12:11:47 +0200

    jwt validation on timeout

--- src/PrivateRoute/PrivateRoute.js
+++ src/PrivateRoute/PrivateRoute.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
+import ajax from "../services/fetchService";
 import { useLocalState } from "../util/useLocalState";
 
 export const PrivateRoute = ({ children }) => {
   /* eslint-disable */
   const [jwt, setJwt] = useLocalState("", "jwt");
+  const [isLoading, setIsLoading] = useState(true);
+  const [isValid, setIsValid] = useState(null);
 
-  return jwt ? children : <Navigate to="/login" />;
+  if (jwt) {
+    ajax(`/api/auth/validate?token=${jwt}`, "get", jwt).then((isValid) => {
+      setIsValid(isValid);
+      setIsLoading(false);
+    });
+  } else {
+    return <Navigate to="/login" />;
+  }
+
+  return isLoading ? (
+    <div>Loading...</div>
+  ) : isValid === true ? (
+    children
+  ) : (
+    <Navigate to="/login" />
+  );
 };
 
 export default PrivateRoute;
